a66c411e4a9a0779c2d2d47152ba4b47d2a7409c
Abrão Astério Junior <abraoastjr@gmail.com>
2022-07-07 21:08:52 -0300

    Operator Precedence and Associativity in C

---
+++ C/C11/Operators, Conditional Statements, & Branching/08-Operator-Precedence-and-Associativity.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include "stdbool.h"
+
+int main()
+{
+    int i = 5 * 10 + 15 * 20;
+    bool b1 = 0xf & 0xa == 0xf ^ 0xa;
+    bool b2 = (0xf & 0xa) == (0xf ^ 0xa);
+
+    printf("i: %i, b1: %i, b2: %i\n", i, b1, b2);
+
+    char c = getchar();
+}
+
+/* OUTPUT:
+    i: 350, b1: 1, b2: 0
+*/
