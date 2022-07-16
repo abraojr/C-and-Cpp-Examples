9f69087827c7553cf29497a669b14784f534c239
Abrão Astério Junior <abraoastjr@gmail.com>
2022-07-16 15:12:43 -0300

    Using Functions to Accept Input and Return Output

---
+++ C/C17/Fundamentals of the C Language/Arrays, Accepting Input, & Returning Output/05-Accepting-Input-and-Returning-Output.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    char character;
+    printf("Enter a character: ");
+
+    character = getchar();
+
+    putchar(character);
+
+    return EXIT_SUCCESS;
+}
+
+/* OUTPUT
+    Enter a character:
+    V
+    V
+*/
