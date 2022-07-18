b319704d239a2a72151bff2995e349ff8b33f4b3
Abrão Astério Junior <abraoastjr@gmail.com>
2022-07-17 22:45:19 -0300

    Creating For Loops in C

---
+++ C/C17/Control Structures in C/Looping & Unconditional Control Structures/02-For-Loops.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 0; i <= 10; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return EXIT_SUCCESS;
+}
+
+/* OUTPUT
+    0
+    1
+    2
+    3
+    4
+    5
+    6
+    7
+    8
+    9
+    10
+*/
