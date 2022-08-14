9d0367878ceb247d6e0ba25abe05811055420b64
Abrão Astério Junior <abraoastjr@gmail.com>
2022-08-14 18:49:30 -0300

    Comparing Equality of Strings

---
+++ C/C17/Advanced Topics in C/Using-Strings-Header-Files-and-Type-Conversions/05-Comparing-Equality-of-Strings/02-strings.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char *string_1 = "Alice";
+    char *string_2 = "Bob";
+
+    int result = strcmp(string_1, string_2);
+
+    printf("Compare result: %d\n", result);
+
+    return EXIT_SUCCESS;
+}
+
+/* OUTPUT
+    Compare result: -1
+*/
---
+++ C/C17/Advanced Topics in C/Using-Strings-Header-Files-and-Type-Conversions/05-Comparing-Equality-of-Strings/04-strings.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    char *string_1 = "Alison";
+    char *string_2 = "Alice";
+
+    int result = strcmp(string_1, string_2);
+
+    printf("Compare result: %d\n", result);
+
+    return EXIT_SUCCESS;
+}
+
+/* OUTPUT
+    Compare result: 16
+*/
