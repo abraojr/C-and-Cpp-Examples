4141988d571cbb087e66f17e65c77afb2a4405fb
Abrão Astério Junior <abraoastjr@gmail.com>
2022-08-14 17:17:45 -0300

    Creating Strings in Character Arrays

---
+++ C/C17/Advanced Topics in C/Using-Strings-Header-Files-and-Type-Conversions/01-Strings-Character-Arrays/01-strings.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    char company_name[] = "Microsoft";
+
+    printf("%s\n", company_name);
+
+    printf("Size of the above string is: %lu\n", sizeof(company_name));
+
+    return EXIT_SUCCESS;
+}
+
+/* OUTPUT
+    Microsoft
+    Size of the above string is: 10
+*/
---
+++ C/C17/Advanced Topics in C/Using-Strings-Header-Files-and-Type-Conversions/01-Strings-Character-Arrays/04-strings.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main()
+{
+    char company_name[5] = {'M', 'i', 'c', 'r', 'o', 's', 'o', 'f', 't', '\0'};
+
+    printf("%s\n", company_name);
+
+    printf("Size of the above string is: %lu\n", sizeof(company_name));
+
+    return EXIT_SUCCESS;
+}
+
+/* OUTPUT
+    Micro
+    Size of the above string is: 5
+*/
