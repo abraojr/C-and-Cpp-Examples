0316027341b71c33b828a416fcf2949cb2da97bb
Abrão Astério Junior <abraoastjr@gmail.com>
2022-08-21 16:12:15 -0300

    Virtual Functions

---
+++ Cpp/Cpp14/Classes--Functions-and-Object-oriented-Programming/03-Virtual-Functions/Source Files/01-virtual-functions.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+using std::cout;
+
+class A
+{
+public:
+    char letter()
+    {
+        return 'A';
+    }
+};
+
+class B : public A
+{
+public:
+    char letter()
+    {
+        return 'B';
+    }
+};
+
+int main()
+{
+    B b;
+    A *a = &b;
+    A &a2 = b;
+
+    cout << "Letter (from a->letter()): " << a->letter() << "\n";
+    cout << "Letter (from b.letter()): " << b.letter() << "\n";
+    cout << "Letter (from a2.letter()): " << a2.letter() << "\n";
+
+    return 0;
+}
+
+/* OUTPUT
+    Letter (from a->letter()): A
+    Letter (from b.letter()): B
+    Letter (from a2.letter()): A
+*/
