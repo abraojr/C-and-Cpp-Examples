dc0f79c32741749931a5efcfe504d52b2ba8e5e7
Abrão Astério Junior <abraoastjr@gmail.com>
2022-08-27 18:57:25 -0300

    Introducing C++ Variables

---
+++ Cpp/Cpp20/Fundamentals of Cpp/Using-Variables-and-Datatypes/01-Variables/01-variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int balance; // Declaring a variable
+
+    cout << "My account balance is : $" << balance << endl;
+
+    balance = 1000; // Initializing a variable
+    cout << "My account balance is : $" << balance << endl;
+
+    return 0;
+}
+
+/* OUTPUT
+    My account balance is : $32517
+    My account balance is : $1000
+*/
---
+++ Cpp/Cpp20/Fundamentals of Cpp/Using-Variables-and-Datatypes/01-Variables/04-variables.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int balance{1000};
+    int withdrawal{200};
+    int final_balance{balance - withdrawal};
+
+    cout << "My current account balance is : $" << balance << endl;
+    cout << "I need to withdraw : $" << withdrawal << endl;
+    cout << "My final account balance is : $" << final_balance << endl;
+
+    return 0;
+}
+
+/* OUTPUT
+    My current account balance is : $1000
+    I need to withdraw : $200
+    My final account balance is : $800
+*/
