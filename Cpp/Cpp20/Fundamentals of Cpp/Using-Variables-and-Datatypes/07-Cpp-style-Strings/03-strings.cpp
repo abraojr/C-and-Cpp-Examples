dd34bd46b5d59a77f0e624d13bdec77d350ec5fc
Abrão Astério Junior <abraoastjr@gmail.com>
2022-08-30 16:07:18 -0300

    Creating Cpp-style Strings

---
+++ Cpp/Cpp20/Fundamentals of Cpp/Using-Variables-and-Datatypes/07-Cpp-style-Strings/03-strings.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+    string proverb_1 = "A bad workman always blames his tools";
+
+    // Initialize one string with the contents of another string
+    string proverb_2{proverb_1};
+
+    // Initialize with 11 characters starting at index 2
+    string proverb_part{proverb_1, 2, 11};
+
+    // Repeat the character 's' 10 times
+    string repeated_s(10, 's');
+
+    cout << "proverb_1: " << proverb_1 << endl;
+    cout << "proverb_2: " << proverb_2 << endl;
+    cout << "proverb_part: " << proverb_part << endl;
+    cout << "repeated_s: " << repeated_s << endl;
+
+    return 0;
+}
+
+/* OUTPUT
+    proverb_1: A bad workman always blames his tools
+    proverb_2: A bad workman always blames his tools
+    proverb_part: bad workman
+    repeated_s: ssssssssss
+*/
