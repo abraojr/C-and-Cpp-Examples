e383f2cc3f8c09036a6c6594c175f15a68a2f8cf
Abrão Astério Junior <abraoastjr@gmail.com>
2022-08-31 12:06:38 -0300

    Indexing into Strings

---
+++ Cpp/Cpp20/Fundamentals of Cpp/Using-Variables-and-Datatypes/10-Indexing-into-Strings/02-indexing-into-strings.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::endl;
+using std::string;
+
+int main()
+{
+    string proverb = "The harder you work, the luckier you get.";
+
+    cout << "Original string: " << proverb << endl;
+
+    proverb[1] = 'H';
+    proverb[2] = 'E';
+
+    cout << "Updated string: " << proverb << endl;
+
+    return 0;
+}
+
+/* OUTPUT
+    Original string: The harder you work, the luckier you get.
+    Updated string: THE harder you work, the luckier you get.
+*/
