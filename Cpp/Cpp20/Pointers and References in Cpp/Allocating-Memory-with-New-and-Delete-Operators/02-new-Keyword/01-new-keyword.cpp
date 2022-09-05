f98bb5e8c97497d63b5d3779ab8b33bee2b514ba
Abrão Astério Junior <abraoastjr@gmail.com>
2022-09-05 10:41:40 -0300

    Allocating Memory Using the new Keyword

---
+++ Cpp/Cpp20/Pointers and References in Cpp/Allocating-Memory-with-New-and-Delete-Operators/02-new-Keyword/01-new-keyword.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    // Initializes revenue_ptr with a nullptr
+    double *revenue_ptr{};
+
+    // Requests memory from the free store for a double variable
+    revenue_ptr = new double;
+
+    // References the memory in the free store and assigns a value
+    *revenue_ptr = 20308.345;
+
+    cout << "Revenue is: " << *revenue_ptr << endl;
+
+    delete revenue_ptr;
+
+    return 0;
+}
+
+/* OUTPUT
+    Revenue is: 20308.3
+*/
---
+++ Cpp/Cpp20/Pointers and References in Cpp/Allocating-Memory-with-New-and-Delete-Operators/02-new-Keyword/03-new-keyword.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    /* Declares double pointer, requests memory from the free store for
+    a double variable and initializes the double variable with a value*/
+    double *revenue_ptr{new double{20308.345}};
+
+    cout << "Revenue is: " << *revenue_ptr << endl;
+
+    delete revenue_ptr;
+
+    return 0;
+}
+
+/* OUTPUT
+    Revenue is: 20308.3
+*/
