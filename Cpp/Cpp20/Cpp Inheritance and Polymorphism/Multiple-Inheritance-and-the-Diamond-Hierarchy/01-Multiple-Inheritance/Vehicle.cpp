179d43345f44bd35ff844199fe694d5e83670cdd
Abrão Astério Junior <abraoastjr@gmail.com>
2022-10-12 13:27:04 -0300

    Understanding Multiple Inheritance

---
+++ Cpp/Cpp20/Cpp Inheritance and Polymorphism/Multiple-Inheritance-and-the-Diamond-Hierarchy/01-Multiple-Inheritance/Vehicle.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <iostream>
+
+#include "Vehicle.h"
+
+using std::cout;
+using std::endl;
+using std::string;
+
+Vehicle::Vehicle(string vehicle_type, float price) : vehicle_type(vehicle_type), price(price) {}
+
+string Vehicle::get_vehicle_type() const
+{
+    return vehicle_type;
+}
+
+float Vehicle::get_price() const
+{
+    return price;
+}
+
+float Vehicle::taxes() const
+{
+    return price * 0.05;
+}
