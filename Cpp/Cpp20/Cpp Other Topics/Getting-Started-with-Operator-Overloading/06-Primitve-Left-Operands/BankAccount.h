f75f2c77156925385eae8cc5232f94fcbd2dce5d
Abrão Astério Junior <abraoastjr@gmail.com>
2022-10-14 15:22:41 -0300

    Overloading Operators with Primitive Left Operands

---
+++ Cpp/Cpp20/Cpp Other Topics/Getting-Started-with-Operator-Overloading/06-Primitve-Left-Operands/BankAccount.h
@@ -0,0 +1,59 @@
+#pragma once
+#include <string>
+
+class BankAccount
+{
+private:
+    int account_id;
+    std::string name;
+    float balance;
+    float cc_due;
+
+public:
+    BankAccount(int account_id, std::string name, float balance);
+
+    std::string get_name() const
+    {
+        return name;
+    }
+
+    float get_balance() const
+    {
+        return balance;
+    }
+
+    bool buy_on_credit(float amount);
+
+    bool pay_off_credit();
+
+    void show() const;
+
+    bool operator<(const BankAccount &other) const
+    {
+        std::cout << "Invoking bool operator<(const BankAccount &other) const" << std::endl;
+        float this_balance = this->balance - this->cc_due;
+        float other_balance = other.balance - other.cc_due;
+
+        return this_balance < other_balance;
+    }
+
+    bool operator<(double other) const
+    {
+        std::cout << "Invoking bool operator<(double other)const" << std::endl;
+
+        float this_balance = this->balance - this->cc_due;
+
+        return this_balance < other;
+    }
+
+    friend bool operator<(double first, const BankAccount &second);
+};
+
+inline bool operator<(double first, const BankAccount &second)
+{
+    std::cout << "Invoking bool operator<(double first, const BankAccount &second)" << std::endl;
+
+    float second_balance = second.balance - second.cc_due;
+
+    return first < second_balance;
+}
