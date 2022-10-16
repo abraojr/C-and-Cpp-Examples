87f635d58ddf8a0728cce95d4f94de6c4728fe34
Abrão Astério Junior <abraoastjr@gmail.com>
2022-10-16 18:40:03 -0300

    Invoking Operators from Other Operators

---
+++ Cpp/Cpp20/Cpp Other Topics/Overloading-Arithmetic-and-Relational-Operators/02-Invoking-Operators-from-Other-Operators/01-example/main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+#include "BankAccount.h"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+    BankAccount carrie_account(1001, "Carrie", 3333.34f);
+    BankAccount troy_account(1002, "Troy", 4325.45f);
+
+    troy_account.buy_on_credit(100);
+
+    carrie_account.show();
+    troy_account.show();
+
+    if (troy_account > carrie_account)
+    {
+        cout << "Troy's balance is greater than Carrie's balance" << endl;
+    }
+
+    cout << "----------------------------" << endl;
+
+    if (troy_account > 1000)
+    {
+        cout << "Troy's balance is greater than $1000" << endl;
+    }
+
+    cout << "----------------------------" << endl;
+
+    if (5333.34 > troy_account)
+    {
+        cout << "$5333.34 is greater than Troy's balance" << endl;
+    }
+
+    return 0;
+}
+
+/* OUTPUT
+    ----------------Carrie----------------
+    Balance: 3333.34 Credit card amount due: 0
+
+    ----------------Troy----------------
+    Balance: 4325.45 Credit card amount due: 100
+
+    Troy's balance is greater than Carrie's balance
+    ----------------------------
+    Troy's balance is greater than $1000
+    ----------------------------
+    $5333.34 is greater than Troy's balance
+*/
