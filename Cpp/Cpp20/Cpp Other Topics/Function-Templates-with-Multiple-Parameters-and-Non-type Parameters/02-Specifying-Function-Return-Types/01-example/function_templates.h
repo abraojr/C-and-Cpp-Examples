50de5f129f20d87ac7f03f21317a327febdb0c2c
Abrão Astério Junior <abraoastjr@gmail.com>
2022-11-10 11:03:37 -0300

    Specifying Function Return Types through Templates

---
+++ Cpp/Cpp20/Cpp Other Topics/Function-Templates-with-Multiple-Parameters-and-Non-type Parameters/02-Specifying-Function-Return-Types/01-example/function_templates.h
@@ -0,0 +1,13 @@
+#pragma once
+
+template <typename TReturn, typename T1, typename T2>
+TReturn sum_of(T1 first, T2 second)
+{
+    return first + second;
+}
+
+template <typename TReturn, typename T1, typename T2>
+TReturn difference_of(T1 first, T2 second)
+{
+    return first - second;
+}
---
+++ Cpp/Cpp20/Cpp Other Topics/Function-Templates-with-Multiple-Parameters-and-Non-type Parameters/02-Specifying-Function-Return-Types/02-example/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+#include "function_templates.h"
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+     cout << "Sum of 10.2 and 1209 = " << sum_of(10.2, 1209) << endl;
+     cout << "Difference of 1235 and 1209 = " << difference_of(1235, 1209) << endl;
+
+     return 0;
+}
+
+/* OUTPUT
+     Sum of 10.2 and 1209 = 1219.2
+     Difference of 1235 and 1209 = 26
+*/
