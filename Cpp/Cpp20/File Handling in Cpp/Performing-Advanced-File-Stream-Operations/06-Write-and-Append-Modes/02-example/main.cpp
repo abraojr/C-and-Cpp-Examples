42ded5c708d7b2c26d9443545f8512799d3056fd
Abrão Astério Junior <abraoastjr@gmail.com>
2022-12-23 21:11:25 -0300

    Understanding the Write and Append Modes

---
+++ Cpp/Cpp20/File Handling in Cpp/Performing-Advanced-File-Stream-Operations/06-Write-and-Append-Modes/02-example/main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::ifstream;
+using std::ofstream;
+using std::string;
+using std::vector;
+
+int main()
+{
+    ofstream output_file_stream;
+
+    output_file_stream.open("names.txt", std::ios::app);
+
+    if (!output_file_stream)
+    {
+        cout << "The file names.txt could not be opened for writing" << endl;
+        return 1;
+    }
+
+    vector<string> string_vector{"Caroline Smith",
+                                 "Richard Jones",
+                                 "Sarah Parker",
+                                 "Chris Evans"};
+
+    for (auto &i : string_vector)
+    {
+        output_file_stream << i << '\n';
+    }
+
+    output_file_stream.close();
+
+    cout << "*******File writing completed*******" << endl;
+
+    return 0;
+}
+
+/* OUTPUT
+    *******File writing completed*******
+*/
