3da353c6c9d8a683fd873975d66f9dfb2b181b12
Abrão Astério Junior <abraoastjr@gmail.com>
2022-12-26 18:55:40 -0300

    Creating Function Pointers

---
+++ Cpp/Cpp20/First-class Functions in Cpp/Defining-and-Using-First-class-Functions/01-Creating-Functions-Pointers/01-example/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+double add(double first_num, double second_num);
+
+double subtract(double first_num, double second_num);
+
+int main()
+{
+    double (*compute_fn)(double, double);
+
+    compute_fn = add;
+
+    cout << "------------Addition------------" << endl;
+    cout << compute_fn(23.987, 65.398) << endl;
+
+    compute_fn = subtract;
+
+    cout << "------------Subtraction------------" << endl;
+    cout << compute_fn(23.987, 65.398) << endl;
+
+    return 0;
+}
+
+double add(double first_num, double second_num)
+{
+    return first_num + second_num;
+}
+
+double subtract(double first_num, double second_num)
+{
+    return first_num - second_num;
+}
+
+/* OUTPUT
+    ------------Addition------------
+    89.385
+    ------------Subtraction------------
+    -41.411
+*/
