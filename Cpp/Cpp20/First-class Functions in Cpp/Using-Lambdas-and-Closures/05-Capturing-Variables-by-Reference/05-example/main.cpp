aadb4e0ce193e47b8ec0f7db8ce889533b266b67
Abrão Astério Junior <abraoastjr@gmail.com>
2022-12-27 11:37:58 -0300

    Capturing Variables by Reference

---
+++ Cpp/Cpp20/First-class Functions in Cpp/Using-Lambdas-and-Closures/05-Capturing-Variables-by-Reference/05-example/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+
+using std::cout;
+using std::endl;
+using std::vector;
+
+template <typename T, typename Aggregate>
+void compute_aggregation(const vector<T> &values, Aggregate aggregate)
+{
+    for (const T &val : values)
+    {
+        aggregate(val);
+    }
+}
+
+template <typename T>
+void print_values(vector<T> values)
+{
+    for (const T &val : values)
+    {
+        cout << val << " ";
+    }
+
+    cout << endl;
+}
+
+int main()
+{
+    vector<int> int_vector{12, 57, 90, 119, 456, 33, 91, 115, 220};
+
+    cout << endl << "--------------Original Values--------------" << endl;
+    print_values(int_vector);
+
+    int treshold = 60;
+
+    float total = 0;
+    int count = 0;
+    float average = 0;
+
+    compute_aggregation(int_vector, [=, &total, &count, &average](const int &elem){
+        if(elem > treshold){
+            total = total + elem;
+            count = count + 1;
+            average = total / count; 
+        } 
+    });
+
+    cout << endl << "--------------Statistics--------------" << endl;
+    cout << "Total: " << total << endl;
+    cout << "Count: " << count << endl;
+    cout << "Average: " << average << endl;
+
+    return 0;
+}
+
+/* OUTPUT
+    --------------Original Values--------------
+    12 57 90 119 456 33 91 115 220
+
+    --------------Statistics--------------
+    Total: 1091
+    Count: 6
+    Average: 181.833
+*/
